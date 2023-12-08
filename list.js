ac6219fcebd9bb916c9c15a71977c64e974b4f90
ANKIT161020 <aab2@somaiya.edu>
2023-12-08 15:44:32 +0530

    finally completed with dark mode for large screens

--- list.js
+++ list.js
@@ -1,27 +1,33 @@
 list = [
   [
     `Marine Lines`,
-    `Marine Lines is a locality in South Mumbai. The name Marine Lines is derived from the Marine Battalion Lines, a military establishment built by the British in the 19th century.[2] The battalion was later converted to an air force residential quarters, and now lies just south of Metro Adlabs. It is also the name of a railway station on the Mumbai suburban railway on the Western Railway railway line. Marine Lines was also the name of the road on which Bombay Hospital and Liberty Cinema stand. It is now known as V Thackersey Marg. Marine Drive is the famous promenade near the station. Marine Drive has one of the finest Art Deco Residential Buildings facing the sea. Built in 1951, it is India's oldest aquarium and is named after Parsi entrepreneur DB Taraporewala, who provided funds for its development. Marine Drive is one of the greatest sites to see the monsoon in Mumbai if you're travelling during the rainy season.`,
-    `./assets/card_img1.jpg`,
+    `A lively area in South Mumbai, Marine Lines is a magnet for tourists. The historic Marine Drive promenade offers panoramic views of the Arabian Sea. The bustling street life, combined with the scenic beauty, makes it a perfect spot for leisurely walks and city exploration.`
   ],
 
   [
     `Bandra-Worli Sea Link`,
-    `The Bandra-Worli Sea Link (officially known as Rajiv Gandhi Sea Link[1]) is a 5.6 km long, 8-lane wide cable-stayed bridge that links Bandra in the Western Suburbs of Mumbai with Worli in South Mumbai. It is the longest sea bridge, as well as the 4th longest bridge in India after Bhupen Hazarika Setu, Dibang River Bridge and Mahatma Gandhi Setu. It contains pre-stressed concrete-steel viaducts on either side. It was planned as a part of the proposed Western Freeway that would link the Western Suburbs to Nariman Point in Mumbai's main business district, but is now planned to become part of the Coastal Road to Kandivali.`,
-    `./assets/card_img2.jpg`,
+    `A modern engineering marvel, the Bandra-Worli Sea Link is a vital link connecting suburbs. Beyond its functional role, it stands as a symbol of Mumbai's progress. Lit up beautifully at night, it offers a stunning view of the cityscape and the Arabian Sea, attracting both locals and tourists.`
   ],
 
   [
     `Elephanta Caves`,
-    `The Elephanta Caves are a collection of cave temples predominantly dedicated to the Hindu god Shiva, which have been designated a UNESCO World Heritage Site.[1][2][3] They are on Elephanta Island, or Gharapuri (literally meaning "the city of caves"),[4] in Mumbai Harbour, 10 kilometres (6.2 mi) east of Mumbai in the Indian state of Mahārāshtra. The island, about 2 kilometres (1.2 mi) west of the Jawaharlal Nehru Port, consists of five Hindu caves, a few Buddhist stupa mounds that date back to the 2nd century BCE,[5][6][7] and two Buddhist caves with water tanks.[8][9]`,
-    `./assets/card_img3.jpg`,
+    `A captivating UNESCO World Heritage Site, the Elephanta Caves boast ancient rock-cut temples with intricate sculptures dedicated to Lord Shiva. Accessible by a short ferry ride from Mumbai, it offers a unique blend of history, spirituality, and natural beauty.`,
   ],
 
   [
-    `CSMT`,
-    `Chhatrapati Shivaji Terminus (officially Chhatrapati Shivaji Maharaj Terminus since 2017, formerly Victoria Terminus, Bombay station code: CSMT (mainline)[4]/ST (suburban)), is a historic railway terminus and UNESCO World Heritage Site in Mumbai, Maharashtra, India.[5]
+    `Chhatrapati Shivaji Maharaj Terminus (CSMT)`,
+    `Formerly Victoria Terminus, CSMT is a masterpiece of Victorian Gothic architecture. Serving as a bustling railway hub, it's not just a transportation center but a historical landmark. The intricately designed building reflects Mumbai's colonial past and enduring charm.`,
+  ],
 
-    The terminus was designed by a British architectural engineer Frederick William Stevens from an initial design by Axel Haig, in an exuberant Italian Gothic style. Its construction began in 1878, in a location south of the old Bori Bunder railway station,[6] and was completed in 1887, the year marking 50 years of Queen Victoria's rule.`,
-    `./assets/card_img4.jpg`,
-  ]
+  [
+    `Gateway of India`,
+    `Mumbai's iconic Gateway of India, overlooking the Arabian Sea, is a must-visit landmark. Built to commemorate a royal visit, it's a bustling hub for tourists and locals alike. The nearby waterfront provides stunning views and a vibrant atmosphere.`,
+  ],
+
+  [
+    `Taj Hotel`,
+    `The Taj Mahal Palace Hotel, a symbol of luxury and resilience, is an integral part of Mumbai's history. Beyond its opulent accommodations, it stands as a testament to the city's strength, having withstood challenges. A visit here combines elegance with historical significance.`,
+  ], 
+
+  [`Juhu Beach`,`Juhu Beach in Mumbai stands as a prominent tourist attraction with its golden sands, lively ambiance, and stunning sunsets. Visitors indulge in beachfront activities, savor local street food, and explore nearby attractions like the Prithvi Theatre, making it a quintessential Mumbai experience and a must-visit destination for tourists.`],
 ];
--- script.js
+++ script.js
@@ -1,80 +1,136 @@
-let parallex_el = document.querySelectorAll(".parallax")
-let text_el = document.querySelector(".text")
-let next = document.querySelector(".next")
-let prev = document.querySelector(".prev")
+let parallex_el = document.querySelectorAll(".parallax");
+let text_el = document.querySelector(".text");
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+let place_name = document.querySelector(".place_name");
+let place_desc = document.querySelector(".place_desc");
+let place_info = document.querySelector(".palce_info");
+let place_img = document.querySelector(".place_img");
+let btns = document.querySelector(".btns");
 
-let xValue=0,yValue =0;
+document.addEventListener("DOMContentLoaded", function () {
+  const checkbox = document.querySelector(".checkbox");
+  const body = document.body;
+  const navH2 = document.querySelector("header nav h2");
 
-function update(){
-  parallex_el.forEach((el)=>{
+
+  checkbox.addEventListener("change", function () {
+    body.classList.toggle("dark");
+    navH2.classList.toggle("dark");
+  });
+});
+
+let xValue = 0,
+  yValue = 0;
+
+function update() {
+  parallex_el.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
-    el.style.transform = `translateX(calc( -50% + ${-xValue * speedx}px)) translateY(calc( -50% + ${yValue * speedy}px))`
-  })
+    el.style.transform = `translateX(calc( -50% + ${
+      -xValue * speedx
+    }px)) translateY(calc( -50% + ${yValue * speedy}px))`;
+  });
 }
 
 update();
 
-window.addEventListener("mousemove",(e)=>{
-  let lastScrollTop = 0;
-  xValue = e.clientX - window.innerWidth/2;
-  yValue = e.clientY - window.innerHeight/2;
+let lastScrollTop = 0;
+window.addEventListener("scroll", (e) => {
+  const scrollPosition = window.scrollY;
 
-  window.addEventListener("scroll",(e)=>{
-    const scrollTop = window.scrollY;
-    if (scrollTop > lastScrollTop) {
-      // Scrolling down
-        text_el.style.transform = `translate(-50%,150%)`;
-    } else {
+  // Get the top position of your divs
+  const placeInfoTop = place_info.offsetTop;
+  const placeImgTop = place_img.offsetTop;
 
-        text_el.style.transform = "translate(-50%, -50%)";
+  if (scrollPosition > lastScrollTop) {
+    // Scrolling down
+    if (scrollPosition > placeInfoTop) {
+      place_info.style.transform = `translate(-60%,0)`;
     }
-  })
 
-update();
-})
+    if (scrollPosition > placeImgTop) {
+      place_img.style.transform = `translate(60%,0)`;
+      btns.style.bottom = `7vh`;
+    }
+
+    text_el.style.transform = `translate(-50%,150%)`;
+  } else {
+    // Scrolling up
+    if (scrollPosition < placeInfoTop) {
+      place_info.style.transform = `translate(-200%,0)`;
+    }
+
+    if (scrollPosition < placeImgTop) {
+      place_img.style.transform = `translate(200%,0)`;
+      btns.style.bottom = `-10vh`;
+    }
+
+    text_el.style.transform = "translate(-50%, -50%)";
+  }
+
+  lastScrollTop = scrollPosition;
+});
+
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2;
+  yValue = e.clientY - window.innerHeight / 2;
+
+  update();
+});
 
 let timeline = gsap.timeline();
 
 Array.from(parallex_el)
-.filter((el)=>!el.classList.contains("text"))
-.forEach((el)=>{
-  timeline.from(
-    el,
+  .filter((el) => !el.classList.contains("text"))
+  .forEach((el) => {
+    timeline.from(
+      el,
+      {
+        top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+        duration: 1.5,
+      },
+      "0"
+    );
+  });
+
+timeline
+  .from(
+    ".text h1",
     {
-    top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
-    duration: 1.5,
+      x: "0",
+      y: "500px",
+      opacity: 0,
+      duration: 2,
     },
-    "0"
+    "1"
+  )
+  .from(
+    ".text h2",
+    {
+      x: "0",
+      y: "-300px",
+      opacity: 0,
+      duration: 1.5,
+    },
+    "1"
   );
-})
 
-timeline.from(
-  ".text h1",
-  {
-    y:"500px",
-    opacity:0,
-    duration:2,
-  },
-  "1"
-).from(
-  ".text h2",
-  {
-    y:"-300px",
-    opacity:0,
-    duration:1.5,
-  },
-  "1"
-)
+let currentindex = 2;
+next.addEventListener("click", () => {
+  let cards = document.querySelectorAll(".card_img");
+  currentindex = (currentindex + 1) % list.length;
+  let places = list[currentindex];
+  place_name.innerHTML = places[0];
+  place_desc.innerHTML = places[1];
+  document.querySelector(".place_img").appendChild(cards[0]);
+});
 
-
-next.addEventListener('click',()=>{
-  let cards = document.querySelectorAll(".card_img")
-  document.querySelector(".place_img").appendChild(cards[0])
-})
-
-
-prev.addEventListener('click',()=>{
-  let cards = document.querySelectorAll(".card_img")
-  document.querySelector(".place_img").prepend(cards[cards.length-1])
-})
+prev.addEventListener("click", () => {
+  let cards = document.querySelectorAll(".card_img");
+  currentindex = (currentindex - 1 + list.length) % list.length;
+  let place = list[currentindex];
+  place_name.innerHTML = place[0];
+  place_desc.innerHTML = place[1];
+  document.querySelector(".place_img").prepend(cards[cards.length - 1]);
+});
